99d1f1a225cac9e6a8b0bfd3f0d365ec5142e0f7
SHT-3756 <gkdlfndkekd@naver.com>
2022-10-21 18:48:12 +0900

    웹소켓 라이브러리 설치

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-console.log("hello");
+// http server
+const server = http.createServer(app);
 
-app.listen(3000);
+// ws server
+const webSocketServer = new WebSocket.Server({ server });
+
+server.listen(3000);
